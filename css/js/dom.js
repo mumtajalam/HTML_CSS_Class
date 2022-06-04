f92a54078ec8565531fd4ba4382fe057846cf5e7
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-06-05 01:03:11 +0530

    html

---
+++ css/js/dom.js
@@ -0,0 +1,70 @@
+// Boolean
+// String
+// Number
+
+// Array
+// Object
+// Map, Set, WeakMap, WeakSet
+
+
+// var sttr = '<div class="header"><div class="logo newclass">Logo</div><div>Search</div><div>Button</div><div>More Options</div><div>Cart</div></div>';
+
+// var arr = ['<div class="header">', [div, h1, span, div, [div, div, [div, div]]]];
+
+// var obj = {
+//     header: {
+//         div: {
+//             h1: 'mumtaj',
+//             h2: 'rbn'
+//         },
+//        div: {
+//            p: 'para',
+//        } 
+//     },
+//     main: {
+//         div: {
+//             h1: ' jhdujjkkh',
+//             h2: 'jdhgh'
+//         },
+//         div: {
+//             p: 'para',
+//         }
+//     },
+//     footer: {
+//         div: {
+//             h1: 'sjhdkdhh',
+//         }
+//     }
+// }
+
+
+// window - browser
+// document - current HTML page
+
+// window.document
+// document
+// DOM - Document Object Model
+
+
+// BOM - Browser Object Model
+// window.history - to store tab history
+
+// window.location - detail of current page URL
+// reload , page Change
+
+// window.console.log('jshdh');
+// window.alert('my name is .....');
+// window.confirm('Are you single.....?');
+
+var ret = window.confirm('Are you single ......?');
+console.log(ret);
+
+if(ret){
+    console.log('you can visite this page');
+}
+else{
+    console.log('you can visite this page');
+}
+
+ var ret2 = window.prompt('Enter your name');
+ console.log(ret2);
